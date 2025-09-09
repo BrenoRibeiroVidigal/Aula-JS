5191d17f766207fb02d738cb44ca6d90300d3412
Breno Ribeiro <brenoribeiro788@gmail.com>
2025-09-09 15:02:01 -0300

    Consertado

--- Lista05/Exercicio03.js
+++ Lista05/Exercicio03.js
@@ -3,9 +3,9 @@ const b = 4;
 const c = 5;
 
 if(a+b > c && a+c>b && b+c>a){
-   if(a == b & b == c && c == a){
+   if(a == b & b == c){
     console.log("É triangulo Equilatero")
-   } else if(a == b & b != c || b == c && c != a){
+   } else if(a == b || a == c || b == c){
     console.log("É triangulo Isosceles")
    } else{
     console.log("É triangulo Escaleno")
