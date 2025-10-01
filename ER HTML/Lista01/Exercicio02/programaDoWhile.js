b10706cfefccd880696e6f4da396b204e92c70bd
Breno Ribeiro <brenoribeiro788@gmail.com>
2025-10-01 20:12:04 -0300

    att

--- ER HTML/Lista01/Exercicio02/programaDoWhile.js
+++ ER HTML/Lista01/Exercicio02/programaDoWhile.js
@@ -5,7 +5,7 @@ function calculoSeno() {
     let a = 0.0;
     do {
         let seno = a - (Math.pow(a, 3) / 6) + (Math.pow(a, 5) / 120) - (Math.pow(a, 7) / 5040);
-        texto += a + "=" + seno + "\n";
+        texto += a + " = " + seno + "\n";
         a += 0.5;
     } while (a <= 3.0);
     return texto;
