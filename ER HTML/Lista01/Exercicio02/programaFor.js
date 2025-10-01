ab98a01e98303523f55bd56bb318e96cbe3d8d9a
Breno Ribeiro <brenoribeiro788@gmail.com>
2025-10-01 20:04:11 -0300

    atualizado

--- ER HTML/Lista01/Exercicio02/programaFor.js
+++ ER HTML/Lista01/Exercicio02/programaFor.js
@@ -2,7 +2,7 @@ const resultado = document.getElementById("idOnResultado");
 
 function calculoSeno() {
     let texto ="";
-    for (let a = 0.0; a <= 3.0; a += 0.5) {
+    for (let a = 0; a <= 3; a += 0.5) {
         let seno = a - (Math.pow(a, 3) / 6) + (Math.pow(a, 5) / 120) - (Math.pow(a, 7) / 5040);
         texto += a + "=" + seno + "\n";
     }
